9a583db3b04be8db6815e1b2f0396af46da9112a
Amir Lima Oliveira <amir.lima45@gmail.com>
2025-03-10 00:30:31 -0700

    ADD: answering more questions of loops in lists.

--- lab_1-Beginning_of_journey/05.02_Training_loops_.py
+++ lab_1-Beginning_of_journey/05.02_Training_loops_.py
@@ -1,9 +1,10 @@
+#                                       LOOPS EXERCISES
 
 # 1. Write a loop to print each character in the string "Python".
 
 word = "python"
 for letters in word:
-    print("spelling:",letters)
+    print("\nspelling:",letters)
 
 # 2. Write a loop to count the number of vowels in the string "data science".
 
@@ -13,8 +14,8 @@ ct = ""
 for l in course:
     if l in vowels:
         ct += l
-print("The number of vowels is:",len(ct))
-print("vowels counted:",ct)
+print("\nThe number of vowels is:",len(ct))
+print("\nvowels counted:",ct)
 
 # 3. Write a loop to reverse the string "hello world" and print the reversed string.
 
@@ -25,8 +26,8 @@ opposite = ""
 for le in s_t_reverse[::-1]:
     opposite += le
 
-print("normal: ",s_t_reverse)
-print("reverse: ",opposite)
+print("\nnormal: ",s_t_reverse)
+print("\nreverse: ",opposite)
 
 # 4. Write a loop to print the ASCII value of each character (search ord() function)
 # in the string "coding".
@@ -37,8 +38,8 @@ ASCII_ = []
 
 for ls in word_list:
     ASCII_.append(ord(ls))
-print(word_list)
-print(ASCII_)
+print("\n",word_list)
+print("\n",ASCII_)
 
 # 5. Write a loop to count the number of times the character "e" appears
 # in the string "experience".
@@ -52,7 +53,7 @@ number_of_e = list()
 for w3 in word3:
     if w3 == let:
         number_of_e.append(w3)
-print("the number of the vowel 'e' is: ",len(number_of_e))
+print("\n","the number of the vowel 'e' is: ",len(number_of_e))
 
 # 6. Write a loop to replace each vowel in the string "education" with an asterisk (*).
 
@@ -68,7 +69,7 @@ for lt in ed:
     else:
         new_word += lt
 
-print(new_word)
+print("\n",new_word)
 
 # 7. Write a loop to print every second character in the string "looping".
 # use range / range only works with integer
@@ -76,7 +77,7 @@ lp = "looping"
 lp_rule = []
 for i in range(1,len(lp),2):
     lp_rule += lp[i]
-print(lp_rule)
+print("\n",lp_rule)
 
 # 8. Write a loop to find the first repeating character in the string "swiss"
 
@@ -87,7 +88,7 @@ for w in word:
     new += word[:]
     if new == w:
         break
-print(new)
+print("\n",new)
  # this is not working
 
 # 9. Write a loop to capitalize each word in the string "machine learning is fun".
@@ -97,22 +98,54 @@ machi_list = list(machi)
 cap_machi = ""
 for i in machi_list:
     cap_machi += (i)
-print(cap_machi)
+print("\n",cap_machi)
 
 # it is possible to add what you want to check in the loop, for i in ->"here"<-
 
 # 10. Write a loop to check if the string "racecar" is the same forwards and backwards (ignore case).
 
+s1 = "racecar"
+str_forward = []
+str_backwards = []
+for i in s1:
+    str_forward.append(i)
+for i in s1[::-1]:
+    str_backwards.append(i)
+if str_forward == str_backwards:
+    print("\nracecar is palindrome")
+else:
+    print("not a palindrom")
 
 # LIST QUESTIONS
 
 # 1. Write a loop to print each element in the list [10, 20, 30, 40, 50].
 
+l1 = [10, 20, 30, 40, 50]
+for i in l1:
+    print("\n",i)
+
 # 2. Write a loop to calculate the sum of all elements in the list [1, 2, 3, 4, 5].
 
+l2 = [1, 2, 3, 4, 5]
+v_list = []
+v = 0
+for obj in l2:
+    v += obj
+    v_list.append(v)
+print("\nThe result of each sum of the values in the list is:",v_list)
+print("\nThe total sum of the list is:", v)
+
 # 3. Write a loop to find the largest number in the list [3, 7, 2, 9, 4].
+l3 = [3, 7, 2, 9, 4]
+max_v = 0
+for obj in l3:
+    if  obj > max_v:
+        max_v = obj
+print("\nlargest number is:",max_v)
 
 # 4. Write a loop to count how many times the number 4 appears in the list [1, 4, 4, 2, 4, 3].
+
+
 
 # 5.Write a loop to create a new list that contains the squares of each number in the list [1, 2, 3, 4, 5].
 
